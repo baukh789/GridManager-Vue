25d566dacbf45dc83649050af291539742c9ead4
baukh789 <182209508@qq.com>
2018-05-30 19:04:15 +0800

    gridmanger fix bug

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,12 @@ import 'gridmanager/css/gm.css';
 import GridManager from 'gridmanager';
 const VueGM = {
     name: 'grid-manager',
-    data: () => {
-      return {
-          gmData: null,
-          gmError: null
-      }
-    },
+    // data: () => {
+    //     return {
+    //         gmData: null,
+    //         gmError: null
+    //     }
+    // },
     props: {
         option: {
             type: Object,
@@ -24,32 +24,71 @@ const VueGM = {
     },
     template: '<table></table>',
     mounted() {
-        console.log('gm-vue mounted');
         const _parent = this.$parent;
+
+        // 包装ajax_success
         const ajax_success = this.option.ajax_success;
         this.option.ajax_success = (respones) => {
-            this.gmData = respones;
+            // this.gmData = respones;
             ajax_success && ajax_success.call(_parent, respones);
         };
 
+        // 包装ajax_error
         const ajax_error = this.option.ajax_error;
         this.option.ajax_error = (error) => {
-            this.gmError = error;
+            // this.gmError = error;
             ajax_error && ajax_error.call(_parent, error);
         };
 
-        // 解析Vue 模版
+        // 解析Vue 模版, data中的row为固定元素
+        // compileList格式为[{th: td element, row: 行数据}]
         this.option.compileVue = (compileList) =>  {
             compileList.forEach(item => {
                 const td = item.td;
+                const attributes = td.firstChild.attributes;
                 new Vue({
                     el: td.firstChild,
-                    data: {
-                        row: item.row
+                    data: () => {
+                        const map = {
+                            row: item.row
+                        };
+                        [].forEach.call(attributes, attr => {
+                            // 当前属性异常或非Vue特定属性
+                            if (!attr.name || !attr.value || !/:|@/g.test(attr.name)) {
+                                return;
+                            }
+
+                            // 当前属性为函数
+                            if (attr.value.indexOf('(') !== -1) {
+                                return;
+                            }
+
+                            // 特定属性不允许变更
+                            if (attrName === 'row') {
+                                console.warn('GridManager warn: Vue attribute row can not be defined!');
+                                return;
+                            }
+                            map[attr.value] = _parent[attr.value];
+                            map[attrName] = _parent[attr.value];
+                        });
+                        return map;
                     },
                     template: td.innerHTML,
                     mounted() {
-                        this.onOpenDialog = _parent.onOpenDialog;
+                        // 改变Vue特性属性所使用的域
+                        [].forEach.call(attributes, attr => {
+                            // 当前属性异常或非Vue特定属性
+                            if (!attr.name || !attr.value || !/:|@/g.test(attr.name)) {
+                                return;
+                            }
+
+                            // 当前属性为函数
+                            if (attr.value.indexOf('(') !== -1) {
+                                const attrSplit = attr.value.split('(');
+                                const fnName = attrSplit[0];
+                                this[fnName] = _parent[fnName];
+                            }
+                        });
                     }
                 });
             });
@@ -65,22 +104,18 @@ const VueGM = {
         });
     },
     beforeCreate() {
-        console.log('gm-vue beforeCreate');
     },
     created() {
-        console.log('gm-vue created');
     },
     updated() {
-        // this.$parent.gmData = this.gmData;
-        console.log('gm-vue updated');
-        this.$nextTick(function () {
-            console.log(this.gmData);
-        })
     },
     destroyed() {
-        console.log('gm-vue destroyed');
+        // 清除右键菜单
+        const menuDomList = document.querySelectorAll('.grid-menu[grid-master]');
+        [].forEach.call(menuDomList, menuDom => {
+            menuDom.parentNode.removeChild(menuDom);
+        });
     }
-    // fn: GridManager
 };
 
 // 支持Vue.use()进行全局注册
